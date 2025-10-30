abad0bb2eb8f23627ec8298adaf5d6bb6e43c7a0
Juliana Pinho <ju_pinho99@hotmail.com>
2025-10-29 22:05:10 -0300

    fix: atualizacao do componente Formulario

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,11 +1,29 @@
-export default function(){
+import { useState } from "react";
+
+export default function Formulario(){
+    const [contatos, setContatos] = useState([])
+    const [maxId, setMaxId] = useState(1)
+
+    function excluir(id){
+        let novoContatos = contatos.filter(contato=>contato.id != id)
+        setContatos(novoContatos)
+    }
+
     function enviar(formData){
         console.log(formData);
         const nome = formData.get("nome")
         const email = formData.get("email")
         const notificar = formData.get("notificar")
         alert(nome + " " + email + " " + notificar)
-        
+        let novoContato = {
+            id:maxId,
+            nome:nome,
+            email:email,
+            notificar:notificar
+        }
+        let novoContatos = [...contatos, novoContato]
+        setContatos(novoContatos)
+        setMaxId(maxId+1)
     }
     return <>
         <form action ={enviar}>
@@ -19,5 +37,26 @@ export default function(){
             <br />
             <button type="submit">Enviar</button>
         </form>
+        <table border={1}>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>E-mail</th>
+                    <th>Notificar</th>
+                    <th>Ação</th>
+                </tr>
+            </thead>
+            <tbody>
+                {contatos.map(contato=><tr>
+                    <td>{contato.id}</td>
+                    <td>{contato.nome}</td>
+                    <td>{contato.email}</td>
+                    <td>{contato.notificar}</td>
+                    <td><button onClick={()=>excluir(contato.id)}>Excluir</button></td>
+                    <td><button>Editar</button></td>
+                </tr>)}
+            </tbody>
+        </table>
     </>
 }
